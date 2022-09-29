29d9db35c11f3a1523bb013d5fbe5ed8c911e221
Khalilnkh <khalilnkh@code.edu.az>
2022-09-29 13:06:57 +0400

    updated

--- assest/main.js
+++ assest/main.js
@@ -1,12 +1,26 @@
 var btn_add=document.getElementById('btn_add');
-var value=document.getElementById('input').value;
+
 
 
 
 btn_add.onclick=function(){
+    var value=document.getElementById('input').value;
     var h1=document.createElement('h1');
+    var icon=document.createElement('i');
+    var div=document.createElement('div')
+    div.setAttribute('class','divc')
+    icon.setAttribute('class','fa-solid fa-trash-can')
     h1.innerHTML=value;
+    // h1.setAttribute('class','h1class')
     var todo=document.getElementById('todo');
-    todo.appendChild(h1)
+    div.appendChild(h1)
+    div.appendChild(icon)
+    todo.appendChild(div)
+
+    var icon=document.querySelector('#todo')
+    icon.addEventListener( `click`,(e)=>{
+        var parent=e.target.parentElement;
+        parent.remove()
+    })
 
 }
